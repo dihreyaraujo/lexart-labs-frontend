e9a8c1520bc0ff3f8620e86c8ba78c6f644389c9
Dihrey Araujo <dihreyh@gmail.com>
2024-03-05 11:18:43 -0300

    implements Login and Register pages

--- lexart-labs/src/pages/Login.js
+++ lexart-labs/src/pages/Login.js
@@ -1,9 +1,104 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Login extends React.Component {
+  state = {
+    username: '',
+    password: '',
+    btnDisable: true,
+    errorMessage: '',
+  }
+
+  constructor() {
+    super();
+    const verifyLoginLogout = () => {
+      const user = JSON.parse(localStorage.getItem('tokenLexart'));
+      console.log(user);
+      if (user) {
+        window.location.assign('http://localhost:3000/products');
+      }
+    }
+    verifyLoginLogout();
+  }
+
+  typeEvent = ({ target }) => {
+    const inputEvent = target.id;
+    this.setState({ [inputEvent]: target.value }, this.enableDisableButton);
+  };
+
+  enableDisableButton = () => {
+    const { username, password } = this.state;
+    if (username && password) {
+      this.setState({ btnDisable: false });
+    } else {
+      this.setState({ btnDisable: true });
+    }
+  };
+
+  verifyUser = async () => {
+    const { username, password } = this.state;
+    const url = 'http://localhost:3001/login'
+    try {
+      const data = await axios.post(url, { username, password });
+      console.log(data.data);
+      const token = data.data.token;
+      localStorage.setItem('tokenLexart', JSON.stringify(token));
+      window.location.assign('http://localhost:3000/products');
+    } catch (e) {
+      this.setState({ errorMessage: e.response.data.message });
+      console.log(e.response);
+    }
+  }
+
   render() {
+    const { username, password, btnDisable, errorMessage } = this.state;
     return (
-      <h1>Login</h1>
+      <div>
+        <h1>Login</h1>
+        <p>Lexart Labs - Processo Seletivo</p>
+        <form>
+          <p>Usuário</p>
+          <label htmlFor='username'>
+            <input
+              type='text'
+              id='username'
+              value= { username }
+              onChange={ (e) => this.typeEvent(e) }
+            />
+          </label>
+          <p>Senha</p>
+          <label htmlFor='password'>
+            <input
+              type='password'
+              id='password'
+              value={ password }
+              onChange={ (e) => this.typeEvent(e) }
+            />
+          </label>
+          <button
+            type='button'
+            disabled={ btnDisable }
+            onClick={ this.verifyUser }
+          >
+            Entrar
+          </button>
+          <p>
+            Ainda não possui uma conta? 
+            <Link to='/register'>
+              Registre-se
+            </Link>
+          </p>
+          {
+            errorMessage
+            ? (
+              <p>
+                { errorMessage }
+              </p>
+            ) : ''
+          }
+        </form>
+      </div>
     )
   }
 }
--- lexart-labs/src/pages/Register.js
+++ lexart-labs/src/pages/Register.js
@@ -1,9 +1,107 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Register extends React.Component {
+  state = {
+    username: '',
+    password: '',
+    btnDisable: true,
+    errorMessage: '',
+  }
+
+  constructor() {
+    super();
+    const verifyLoginLogout = () => {
+      const user = JSON.parse(localStorage.getItem('tokenLexart'));
+      if (user) {
+        window.location.assign('http://localhost:3000/products');
+      }
+    }
+    verifyLoginLogout();
+  }
+
+  typeEvent = ({ target }) => {
+    const inputEvent = target.id;
+    this.setState({ [inputEvent]: target.value }, this.enableDisableButton);
+  };
+
+  enableDisableButton = () => {
+    const { username, password } = this.state;
+    if (username && password) {
+      this.setState({ btnDisable: false });
+    } else {
+      this.setState({ btnDisable: true });
+    }
+  };
+
+  verifyUser = async () => {
+    const { username, password } = this.state;
+    console.log('CHAMOU VERIFY')
+    const url = 'http://localhost:3001/register';
+    try {
+      const data = await axios.post(url, { username, password });
+      console.log('teste: ', data.data);
+      if (data.response.data.message === 'Usuário criado com sucesso.') {
+        window.location.assign('http://localhost:3000/login');
+      } else {
+        this.setState({ errorMessage: data.response.data.message });
+      }
+    } catch (e) {
+      console.log('teste: ', e.response);
+      this.setState({ errorMessage: e.response.data.message });
+    }
+  }
+
   render() {
+    const { username, password, btnDisable, errorMessage } = this.state;
     return (
-      <h1>Register</h1>
+      <div>
+        <h1>Register</h1>
+        <p>Lexart Labs - Processo Seletivo</p>
+        <form>
+          <p>Usuário</p>
+          <label htmlFor='username'>
+            <input
+              type='text'
+              id='username'
+              value= { username }
+              onChange={ (e) => this.typeEvent(e) }
+            />
+          </label>
+          <p>Senha</p>
+          <label htmlFor='password'>
+            <input
+              type='password'
+              id='password'
+              value={ password }
+              onChange={ (e) => this.typeEvent(e) }
+            />
+          </label>
+          <br />
+          <button
+            type='button'
+            disabled={ btnDisable }
+            onClick={ this.verifyUser }
+          >
+            Registrar
+          </button>
+          <p>
+            Já possui uma conta? 
+            <Link to='/'>
+              Entre
+            </Link>
+          </p>
+          {
+            errorMessage
+            ? (
+              <p>
+                { errorMessage }
+              </p>
+            ) : ''
+          }
+        </form>
+      </div>
     )
   }
 }
