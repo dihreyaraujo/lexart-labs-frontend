1d56241cc1c089fd9eec0699cc2513e83b8fd013
Dihrey Araujo <dihreyh@gmail.com>
2024-04-10 18:22:41 -0300

    Update index.js

--- lexart-labs/src/index.js
+++ lexart-labs/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 ReactDOM.render(
   <BrowserRouter>
